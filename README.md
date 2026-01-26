2956ac04fc828816c37fe035a6d8ddc950d3d938
Andertius <pylypchuk.serhiy@gmail.com>
2026-01-26 23:36:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Memento
+
+A lightweight reboot of a school project about flash cards
---
+++ src/Memento.Domain/Models/Category.cs
@@ -0,0 +1,12 @@
+namespace Memento.Domain.Models;
+
+public sealed class Category
+{
+	public int Id { get; set; }
+	
+	public string? Name { get; set; }
+	
+	public string? Description { get; set; }
+	
+	public string? Image { get; set; }
+}
